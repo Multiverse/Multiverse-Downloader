568e0db839cbdab6aa2502fa319e9b81829f4bfd
Jeremy Wood <farachan@gmail.com>
2012-12-13 09:52:44 -0500

    Added cleaner names to VersionType.

--- src/main/java/com/mvplugin/downloader/api/VersionType.java
+++ src/main/java/com/mvplugin/downloader/api/VersionType.java
@@ -4,7 +4,22 @@ package com.mvplugin.downloader.api;
  * The different version types available.
  */
 public enum VersionType {
-    ALPHA,
-    BETA,
-    RELEASE
+    ALPHA("Alpha"),
+    BETA("Beta"),
+    RELEASE("Release");
+
+    private final String name;
+
+    VersionType(final String name) {
+        this.name = name;
+    }
+
+    /**
+     * Gets the human readable name of this version type.
+     *
+     * @return the human readable name of this version type.
+     */
+    public String getName() {
+        return name;
+    }
 }
